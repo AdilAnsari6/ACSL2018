f7609071d3d4afd874b606cd805bb2231e6dacef
Unknown <adilabbas6@yahoo.com>
2018-12-19 15:38:51 -0500

    finished

--- ACSL 2018/Program.cs
+++ ACSL 2018/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Numerics;
 
 namespace ACSL_2018
 {
@@ -9,29 +10,31 @@ namespace ACSL_2018
         {
             string[] lines = File.ReadAllLines("sr-sample-input.txt");
             string line;
+            string padded;
+            BigInteger number;
+            int pad;
             int split;
-            long number;
-            int sum = 0;
             for(int i = 0; i<lines.Length;i++)
             {
-                int j = 0;
+                int sum = 0;
                 line = lines[i];
                 string[] parts = line.Split(' ');
-                while(parts[0].Length % parts[1].Length != 0)
-                {
-                    parts[0].PadRight(parts[0].Length+1, '0');
-                }
                 split = Convert.ToInt32(parts[1]);
-                number = Convert.ToInt64(parts[0]);
-                while (number != 0)
+                if (parts[0].Length % split != 0)
+                    pad = split - (parts[0].Length % split);
+                else
+                    pad = 0;
+                padded = parts[0].PadRight(parts[0].Length+pad,'0');
+                number = BigInteger.Parse(padded);
+                while(number != 0)
                 {
-                    if (j != 0)
-                        sum += (int)(number % (long)Math.Pow(10, split * (j + 1))) / (int)Math.Pow(10, split * j);
-                    else
-                        sum += (int)(number % (long)Math.Pow(10, split * (j + 1)));
-                    number -= sum;
-                    j++;
+                    sum += (int)(number % (BigInteger)Math.Pow(10, split));
+                    padded = padded.Substring(0, padded.Length - split);
+                    if (padded == "")
+                        break;
+                    number = BigInteger.Parse(padded);
                 }
+                Console.WriteLine(sum);
             }
         }
     }
